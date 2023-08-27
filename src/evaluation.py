049abb897174e9aa702ebcb2998d11d8c3c69447
sann-htet <sannhtet899@gmail.com>
2023-08-27 16:18:22 +0630

    evaluating the model accuracy

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-def evaluate_model(model, test_data, test_labels):
-    test_loss, test_acc = model.evaluate(test_data, test_labels)
+def evaluate_model(model, test_data):
+    test_loss, test_acc = model.evaluate(test_data)
     
     return test_acc
